1259591b7a7652f873f076c4c3eaa9d4d8b4ee45
Amandalfs <amandalfs57@gmail.com>
2023-10-27 12:48:17 -0300

    refactor: add to react form hook and zod by register modal

--- src/components/modals/RegisterCompanyModal/RegisterCompanyModal.tsx
+++ src/components/modals/RegisterCompanyModal/RegisterCompanyModal.tsx
@@ -3,34 +3,40 @@ import close from '../../../assets/close.svg';
 import trash from '../../../assets/lixo.svg';
 import { useModals } from '../../../hooks/useModals';
 
-import { useState } from 'react';
 import { useCompanies } from '../../../hooks/useCompanies';
-
-interface Form {
-    name: string;
-    email: string;
-    cnpj: string;
-}
+import { useForm } from 'react-hook-form';
+import * as zod from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 
 export function RegisterCompanyModal(){
-    const [form, setForm] = useState<Form>({
-        name: '',
-        email: '',
-        cnpj: '',
-    })
-
-    function cleanForm(){
-        setForm({
-            name: '',
-            email: '',
-            cnpj: '',
-        })
-    }
-
 
     const { closeModalRegister } = useModals();
     const { registerCompany } = useCompanies();
+
+    const dataRegisterCompanyValidSchema = zod.object({
+        name: zod.string().min(1, "Preencha todos os campos"),
+		email: zod.string().email("Email precisa ser valido"),
+		cnpj: zod.string().min(8, "Preencha todos os campos"),
+	});
+		
+		type IDataRegisterCompanyValidSchema = zod.infer<typeof  dataRegisterCompanyValidSchema>
+
+		const { register, handleSubmit, formState: { errors }, reset } = useForm<IDataRegisterCompanyValidSchema>({
+			resolver: zodResolver(dataRegisterCompanyValidSchema),
+			defaultValues: {
+                name: '',
+                cnpj: '',
+                email: '',
+			}       
+		});
+
+
+        function handleSuccess(data: IDataRegisterCompanyValidSchema){
+            registerCompany(data);
+            reset();
+            closeModalRegister();
+		}
 
     return (<>
     <div className={styles.modal}>
@@ -39,7 +45,7 @@ export function RegisterCompanyModal(){
             <img src={close} alt="" 
                 onClick={()=>{
                     closeModalRegister();
-                    cleanForm();
+                    reset();
                 }}
             />
         </header>
@@ -47,27 +53,39 @@ export function RegisterCompanyModal(){
         <div className={styles.inputs}>
             <div className={styles.input}>
                 <label>Nome</label>
-                <input type="text" name="" id="" 
-                    onChange={(e) => {
-                        setForm((prevState) => ({...prevState, name: e.target.value}))
-                    }}
-                />
+                <input type="text" id="" 
+                    {...register('name')}
+                /> 
+                {
+                    errors.name &&   
+                     <div className={styles.errorMessage}>
+                        {errors.name.message}
+                    </div>
+                }
             </div>
             <div className={styles.input}>
                 <label>CNPJ</label>
-                <input type="text" name="" id="" 
-                     onChange={(e) => {
-                        setForm((prevState) => ({...prevState, cnpj: e.target.value}))
-                    }}
+                <input type="text" id="" 
+                    {...register('cnpj')}
                 />
+                {
+                    errors.cnpj &&   
+                    <div className={styles.errorMessage}>
+                        {errors.cnpj.message}
+                    </div>
+                }
             </div>
             <div className={styles.input}>
                 <label>E-mail</label>
-                <input type="text" name="" id="" 
-                     onChange={(e) => {
-                        setForm((prevState) => ({...prevState, email: e.target.value}))
-                    }}
+                <input type="text" id="" 
+                    {...register('email')}
                 />
+                {
+                    errors.email &&   
+                    <div className={styles.errorMessage}>
+                        {errors.email.message}
+                    </div>
+                }
             </div>
         </div>
 
@@ -77,15 +95,11 @@ export function RegisterCompanyModal(){
                 <button className={styles.btnCancel}
                     onClick={()=>{
                         closeModalRegister();
-                        cleanForm();
+                        reset();
                     }}
                 >Cancelar</button>
                 <button className={styles.btnRegister}
-                    onClick={async ()=>{
-                        await registerCompany(form);
-                        cleanForm();
-                        closeModalRegister();
-                    }}
+                    onClick={handleSubmit(handleSuccess)}
                 >Cadastrar</button>
             </div>
         </div>
@@ -93,7 +107,7 @@ export function RegisterCompanyModal(){
     <div className={styles.filter} 
         onClick={()=>{
             closeModalRegister();
-                    cleanForm();
+            reset();
         }}
     />
     </>)
